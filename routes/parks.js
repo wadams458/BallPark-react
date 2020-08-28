c24692f329e543f75fbc1945945f033330baab0c
wadams <wwadams94@gmail.com>
2020-08-27 21:52:45 -0700

    yeah... mvp...mvp...

--- routes/parks.js
+++ routes/parks.js
@@ -9,7 +9,7 @@ router.get("/:id", ctrl.parks.show);
 // router.post("/", authRequired, ctrl.parks.create);
 router.post("/", ctrl.parks.create);
 router.put("/:id", authRequired, ctrl.parks.update);
-router.delete("/:id", authRequired, ctrl.parks.destroy);
+router.delete("/:id", ctrl.parks.destroy);
 
 // exports
 module.exports = router;
